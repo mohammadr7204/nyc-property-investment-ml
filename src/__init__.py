425ec7e2c3ebb34b9cb4fe6ba2fca8f84be9572d
mohammadr7204 <mohammadr7204@gmail.com>
2025-06-05 01:21:12 -0400

    Add src package initialization

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+"""
+NYC Property Investment ML - Source Package
+"""
+
+__version__ = "1.0.0"
+__author__ = "Mohammad R"
+__description__ = "AI-powered NYC property investment analysis system"
+
+from .analyzer import NYCPropertyInvestmentAnalyzer
+from .data_pipeline import NYCPropertyDataPipeline, PropertyData
+from .ml_model import NYCRevenuePredictor
+
+__all__ = [
+    'NYCPropertyInvestmentAnalyzer',
+    'NYCPropertyDataPipeline', 
+    'PropertyData',
+    'NYCRevenuePredictor'
+]
